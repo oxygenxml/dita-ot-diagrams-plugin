b635577e5d2e252e779cbb666732c7e08ff55914
Julien Lacour <julien@sync.ro>
2024-12-05 09:14:49 +0200

    Format and indent

--- com.oxygenxml.diagrams.svg/src/com/oxygenxml/mermaid/URLUtils.java
+++ com.oxygenxml.diagrams.svg/src/com/oxygenxml/mermaid/URLUtils.java
@@ -10,26 +10,26 @@ import net.sf.saxon.lib.ExtensionFunctionDefinition;
 import net.sf.saxon.om.Sequence;
 import net.sf.saxon.om.StructuredQName;
 import net.sf.saxon.trans.XPathException;
+import net.sf.saxon.value.BooleanValue;
 import net.sf.saxon.value.SequenceType;
 import net.sf.saxon.value.StringValue;
-import net.sf.saxon.value.BooleanValue;
 
 public class URLUtils extends ExtensionFunctionDefinition {
 
-	  @Override
-	  public SequenceType[] getArgumentTypes() {
-	    return new SequenceType[] { SequenceType.SINGLE_STRING};
-	  }
+	@Override
+	public SequenceType[] getArgumentTypes() {
+		return new SequenceType[] { SequenceType.SINGLE_STRING };
+	}
 
-	  @Override
-	  public StructuredQName getFunctionQName() {
-	    return new StructuredQName("URLUtils", "java:com.oxygenxml.mermaid.URLUtils", "check");
-	  }
+	@Override
+	public StructuredQName getFunctionQName() {
+		return new StructuredQName("URLUtils", "java:com.oxygenxml.mermaid.URLUtils", "check");
+	}
 
-	  @Override
-	  public SequenceType getResultType(SequenceType[] suppliedArgumentTypes) {
-	    return SequenceType.SINGLE_BOOLEAN;
-	  }
+	@Override
+	public SequenceType getResultType(SequenceType[] suppliedArgumentTypes) {
+		return SequenceType.SINGLE_BOOLEAN;
+	}
 
 	@Override
 	public ExtensionFunctionCall makeCallExpression() {
@@ -39,15 +39,15 @@ public class URLUtils extends ExtensionFunctionDefinition {
 			public Sequence call(XPathContext arg0, Sequence[] arguments) throws XPathException {
 				try {
 					String urlString = ((StringValue) arguments[0].iterate().next()).getStringValue();
-          HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
-          connection.setRequestMethod("HEAD");
-          connection.setConnectTimeout(3000);
-          connection.setReadTimeout(3000);
-          int responseCode = connection.getResponseCode();
-          return BooleanValue.get(responseCode >= 200 && responseCode < 400);
-        } catch (IOException e) {
-          return BooleanValue.FALSE;
-        }
+					HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
+					connection.setRequestMethod("HEAD");
+					connection.setConnectTimeout(3000);
+					connection.setReadTimeout(3000);
+					int responseCode = connection.getResponseCode();
+					return BooleanValue.get(responseCode >= 200 && responseCode < 400);
+				} catch (IOException e) {
+					return BooleanValue.FALSE;
+				}
 			}
 		};
 	}
